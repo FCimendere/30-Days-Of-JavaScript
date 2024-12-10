4afd7e0ed28cd705cd515ba7d8ef02d5ffc104e0
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-12-10 15:14:57 +0100

    solved day 1

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,33 @@
+let stringName = "Fly";
+let newName = "Holly";
+let isThisCorrect = false;
+let num = 10;
+let maidenName = null;
+let notThereData = undefined;
+var test;
+
+console.log(typeof stringName);
+console.log(typeof newName);
+console.log(typeof isThisCorrect);
+console.log(typeof maidenName);
+console.log(typeof notThereData);
+console.log(typeof test);
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+/*let firstName = 'Fulya';
+let lastName = "test";
+let maritalStatus = `married`;
+let country = 'Netherland';
+let age = 38;*/
+
+let firstName = "fulya",
+  lastName = "test",
+  maritalStatus = "married",
+  country = "NL",
+  age = 38,
+  myAge = 38,
+  yourAge = 25;
+
+console.log(`My age is ${myAge} years old.`);
+console.log("Your age is" + " " + yourAge + " " + "is old.");
